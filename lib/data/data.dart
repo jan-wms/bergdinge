c8b2594bca3c5b49c8b19e51fcd789bf167d82de
jan2210 <jan.wermeckes@gmx.de>
2023-04-02 13:14:46 +0200

    data: categories

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -13,52 +13,246 @@ class Data {
   final _categories = {
     'Bekleidung': {
       'Jacken': {
-        'Hardshelljacke',
-        'Softshelljacke',
-        'Isolationsjacke',
-        'Fleecejacke',
-        'Woll- & Merinojacken',
-        'Windbreaker',
-        'Skijacke',
-        'Poncho',
-        'Sonstige',
+        'Hardshelljacke': null,
+        'Softshelljacke': null,
+        'Isolationsjacke': null,
+        'Windbreaker': null,
+        'Skijacke': null,
+        'Poncho': null,
+        'Sonstige': null,
       },
       'Hosen': {
-        'Skihose',
-        'Trekkinghose',
-        'Hochtourenhose',
-        'Kletterhose',
-        'Hardshellhose',
-        'Winterhose',
-        'Softshellhose',
-        'Laufhose',
-        'Trainingshose',
-        'Skitourenhose',
-        'Fleecehose',
-        'Isolationshose',
-        'Tights',
-        'Shorts',
+        'Skihose': null,
+        'Trekkinghose': null,
+        'Hochtourenhose': null,
+        'Kletterhose': null,
+        'Hardshellhose': null,
+        'Softshellhose': null,
+        'Laufhose': null,
+        'Skitourenhose': null,
+        'Isolationshose': null,
+        'Shorts': null,
       },
+      'Pullover & Hoodies': {
+        'Fleecejacke': null,
+        'Woll- & Merinojacke': null,
+        'Hoodie': null,
+        'Pullover': null,
+        'Sonstige': null,
+      },
+      'T-Shirts & Longsleeves': {
+        'Longsleeve': null,
+        'T-Shirt': null,
+        'Hemd': null,
+        'Tanktop': null,
+        'Sonstige': null,
+      },
+      'Weste': null,
+      'Funktionsunterwäsche': {
+        'Unterhose': null,
+        'Lange Unterhose': null,
+        'Unterhemd': null,
+        'BH': null,
+        'Sonstige': null,
+      },
+      'Socken': {
+        'Skisocken': null,
+        'Wandersocken': null,
+        'Laufsocken': null,
+        'Sonstige': null,
+      },
+      'Accessories': {
+        'Handschuhe': null,
+        'Multifunktionstücher & Schals': null,
+        'Gürtel & Hosenträger': null,
+        'Gamaschen': null,
+        'Kopfbedeckung': {
+          'Mütze': null,
+          'Stirnband': null,
+          'Sturmhaube': null,
+          'Hut': null,
+          'Cap': null,
+          'Sonstige': null,
+        },
+        'Brillen': {
+          'Sonnenbrille': null,
+          'Gletscherbrille': null,
+          'Sicherungsbrillen': null,
+          'Skibrille': null,
+          'Sonstige': null,
+        },
+        'Sonstige': null
+      }
     },
-    'Schuhe': null,
+    'Schuhe': {
+      'Wander- & Bergschuhe': null,
+      'Winterschuhe': null,
+      'Zustiegsschuhe': null,
+      'Barfussschuhe': null,
+      'Hüttenschuhe': null,
+      'Kletterschuhe': null,
+      'Sandalen': null,
+      'Trail- & Laufschuhe': null,
+      'Sonstige': null,
+    },
     'Ausrüstung': {
-      'Rucksäck': {
-        'Wanderrucksack',
-        'Kletterrucksack',
-        'Hochtourenrucksack',
-        'Lawinenrucksack',
-        'Trailrunningweste',
-      },
+  'Messer & Werkzeuge': {
+  'Äxte & Beile': null,
+  'Feststehende Messer': null,
+  'Taschenmesser und Multitools': null,
+  'Sonstige': null,
+  },
+  'Orientierung & Technik': {
+  'GPS-Gerät': null,
+  'Kompass': null,
+  'Stirn & Taschenlampe': null,
+  'Batterien & Akkus': null,
+  'Kabel & Stecker': null,
+  'Kamera': null,
+  'Smartphone': null,
+  'Uhr': null,
+  'Bücher & Karten': {
+  'Kletterführer': null,
+  'Trainings- & Lehrbücher': null,
+  'Wanderführer': null,
+  'Kartenmaterial': null,
+},
+  'Sonstige':null,
+  },
+  'Klettern': {
+  'Kletterhelm': null,
+  'Klettergurt': null,
+  'Klettersteigset': null,
+  'Hochtouren- & Eisausrüstung': {
+  'Steigeisen': null
+  'Eispickel': null,
+  'Eisschrauben': null,
+  },
+  'Sicherungs- & Abseilgeräte': null,
+
+
+  'Karabiner
+  Karabiner & Expressen
+  Seile, Schlingen & Reepschnüre
+  Keile, Friends & Haken
+  KARABINER & EXPRESS-SETS
+  Express-Sets
+  HMS-Karabiner
+  Schnappkarabiner
+
+  KLETTERSEILE
+  Einfachseile
+  Halbseile
+  Seilsäcke & Seilzubehör
+
+  'Kletter- & Boulderzubehör': {
+  'Chalkbags': null,
+  'Chalk': null,
+  'Kletter- & Boulderbürsten': null,
+  'Crashpads': null,
+}
+  SCHLINGEN & REEPSCHNUR
+  Rundschlingen
+  Reepschnur
+  Express-Schlingen
+
+  BIGWALL & TRADCLIMBING
+  Klemmgeräte & Friends
+  Klemmkeile
+  Steigklemmen
+  },
+'Schlafen & Liegen': {
+'Zelt': null,
+'Tarp':null,
+'Hängematte': null,
+'Schlafsack': null,
+'Isomatte': null,
+'Decken': null,
+'Kissen':null,
+'Moskitonetze': null,
+'Biwaksack': null,
+},
+  'Outdoor Küche': {
+'Trinkflaschen & Wasserträger': {
+'Trinkflasche': null,
+'Thermoskanne': null,
+'Trinkblase': null,
+},
+'Kocher': null,
+'Töpfe & Pfannen': null,
+'Geschirr & Besteck': null,
+'Behälter': null,
+'Grill & Feuer': null,
+},
+
+
+
+
+  Freerideski
+  Freeridebindungen
+  Freerideskischuhe
+  Langlaufausrüstung
+  Langlaufski
+  Langlaufbindungen
+  Langlaufschuhe
+  Langlaufstöcke
+
+'Wintersportausrüstung': {
+  'Schneeschuhe': null,
+  'Rodel': null,
+  'Grödel & Spikes': null,
+  'Skihelme & Protektoren': null,
+'Skitour': {
+'Skifelle': null,
+'Tourenski': null,
+'Tourenskischuhe': null,
+'Tourenbindungen': null,
+'Harscheisen': null,
+},
+},
+STÖCKE
+  Trekkingstöcke
+  Trailrunning Stöcke
+  Nordic Walking Stöcke
+
+Insekten- & Sonnenschutz
+Hygiene
+Wasseraufbereitung
+Handtücher
+Pflegeprodukte
+  REISEZUBEHÖR & PFLEGEMITTEL
+  Mikrofaserhandtücher
+  Drogerie & Körperpflege
+
+  'Rucksäcke & Taschen': {
+'Rucksäcke': {
+  'Wanderrucksack': null,
+  'Kletterrucksack': null,
+  'Hochtourenrucksack': null,
+  'Lawinenrucksack': null,
+  'Trailrunningweste': null,
+  'Kraxe': null,
+  },
+  'Taschen': null,
+  'Packsäcke': null,
+  'Schutzhülle': null,
+  },
       'Notfallausrüstung': {
         'Erste Hilfe',
-        'Lawinenausrüstung',
+        'Lawinenausrüstung': {
+  'LVS-Geräte': null,
+  'Schaufeln': null,
+  'Sonden': null,
+  },
         'Biwaksack',
       },
-
+    },
+    'Verpflegung': {
+      'Getränke': null,
+      'Nahrung': null,
     },
   };
 
   List<String> get sports => _sports;
-
-  Map<String, Map<String, Set<String>?>?> get categories => _categories;
+  Map<String, dynamic> get categories => _categories;
 }
