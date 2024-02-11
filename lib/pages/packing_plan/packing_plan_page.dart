59a98a7d06d48baef7c7fd5df09684541b184ddb
jan2210 <jan.wermeckes@gmx.de>
2024-02-11 10:19:37 +0100

    2024-02-11

--- lib/pages/packing_plan/packing_plan_page.dart
+++ lib/pages/packing_plan/packing_plan_page.dart
@@ -4,7 +4,7 @@ import 'package:equipment_app/pages/packing_plan/packing_plan_card.dart';
 import 'package:equipment_app/pages/packing_plan/packing_plan_edit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+import 'package:shimmer/shimmer.dart';
 import '../../data/design.dart';
 import '../../data/providers.dart';
 
@@ -20,16 +20,57 @@ class PackingPlanPage extends ConsumerWidget {
       child: CustomScrollView(slivers: <Widget>[
         CustomAppBar(
           title: 'Packlisten',
-          onAddButtonPressed: () => CustomDialog.showCustomModal(context: context, child: const PackingPlanEdit()),
+          onAddButtonPressed: () => CustomDialog.showCustomModal(
+              context: context, child: const PackingPlanEdit()),
           onChanged: (_) {},
         ),
         packingPlanList.when(
           error: (error, stackTrace) =>
               SliverToBoxAdapter(child: Text(error.toString())),
-          loading: () => const SliverToBoxAdapter(
-              child: CircularProgressIndicator.adaptive()),
+          loading: () => SliverList.separated(
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              return Shimmer.fromColors(
+                baseColor: Colors.grey.shade300,
+                highlightColor: Colors.grey.shade100,
+                child: ListTile(
+                  enabled: false,
+                  onTap: () {},
+                  title: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        height: 17.0,
+                        width: 250.0,
+                        decoration: const BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(2.0))),
+                      )),
+                  subtitle: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        height: 10.0,
+                        width: 150.0,
+                        decoration: const BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(1.0))),
+                      )),
+                  trailing: const Icon(Icons.chevron_right_rounded),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const Padding(
+                padding: Design.pagePadding,
+                child: Divider(
+                  height: 0.0,
+                ),
+              );
+            },
+          ),
           data: (data) {
-            if(data.isEmpty) {
+            if (data.isEmpty) {
               return const SliverFillRemaining(
                 hasScrollBody: false,
                 child: Center(child: Text('Erstelle deine erste Packliste.')),
