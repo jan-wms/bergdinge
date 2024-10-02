95b72b0fd7f4c538cd8d5206e04646a451b9c68a
jan-wms <jan.wermeckes@gmx.de>
2024-10-02 22:15:50 +0200

    02-10-2024

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:equipment_app/custom_widgets/custom_appbar.dart';
 import 'package:equipment_app/data_models/article.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import '../../data/design.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,11 +18,11 @@ class _HomePageState extends State<HomePage> {
       title: 'Rucksack richtig packen',
       subTitle: 'Tipps',
       imageProvider: const AssetImage('assets/articles/backpack.jpg'),
-      color: const Color.fromRGBO(52, 94, 50, 1.0),
+      color: const Color.fromRGBO(35, 63, 33, 1.0),
       content: [
         const Text(
           'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergrenluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo  et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ins et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-        style: Design.articleTextStyle,
+          style: Design.articleTextStyle,
         )
       ],
     ),
@@ -33,16 +33,37 @@ class _HomePageState extends State<HomePage> {
       color: const Color.fromRGBO(50, 71, 94, 1.0),
       content: [
         const Text(
-          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolor, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea reb',
+          'Bei der AlpenAcademy lernst du alles rund ums Thema Bergsport. Meine Kurse sollen dir alles vermitteln, was man so braucht, um sicher, erfolgreich und bequem den schönsten Sport der Welt genießen zu können. Im Detail geht es um die Themen Klettern, Bergsteigen, Skitouren, Technik, Tourenguides und Ausrüstung! Du wirst auf meinem Kanal bestimmt fündig.',
           style: Design.articleTextStyle,
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 30.0,bottom: 30.0),
+          child: TextButton(
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('youtube.com/@alpenacademy',style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600
+                  ),),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Icon(Icons.launch_rounded),
+                  )
+                ],
+              ),
+              onPressed: () async {
+                final Uri url = Uri.parse('https://www.youtube.com/@alpenacademy/');
+                launchUrl(url);
+              }),
+        ),
       ],
     ),
     Article(
       title: 'Rucksack richtig einstellen',
       subTitle: 'Ratgeber',
+      color: const Color.fromRGBO(91, 94, 91, 1.0),
       imageProvider: const AssetImage('assets/articles/landscape2.jpg'),
-      color: const Color.fromRGBO(35, 63, 33, 1.0),
       content: [
         const Text(
           'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergrenluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo  et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ins et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
@@ -113,7 +134,10 @@ class _ArticleCard extends StatelessWidget {
                         image: DecorationImage(
                             image: article.imageProvider, fit: BoxFit.cover),
                         borderRadius: BorderRadius.circular(20),
-                        border: Border.all(color: article.color, style: BorderStyle.solid, width: 4.0),
+                        border: Border.all(
+                            color: article.color,
+                            style: BorderStyle.solid,
+                            width: 4.0),
                         boxShadow: [
                           BoxShadow(
                             color: Colors.grey.withOpacity(0.2),
@@ -128,7 +152,8 @@ class _ArticleCard extends StatelessWidget {
                         padding: const EdgeInsets.all(20.0),
                         decoration: BoxDecoration(
                           color: article.color,
-                          borderRadius: const BorderRadius.vertical(bottom: Radius.circular(16.0)),
+                          borderRadius: const BorderRadius.vertical(
+                              bottom: Radius.circular(16.0)),
                         ),
                         child: Text(
                           article.title,
@@ -140,16 +165,18 @@ class _ArticleCard extends StatelessWidget {
                       ),
                     ),
                     Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 20.0),
                       decoration: BoxDecoration(
                         color: article.color,
-                        borderRadius: const BorderRadius.only(topLeft: Radius.circular(16.0), bottomRight: Radius.circular(16.0)),
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16.0),
+                            bottomRight: Radius.circular(16.0)),
                       ),
                       child: Text(
                         article.subTitle,
-                        style: const TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16),
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 16),
                       ),
                     ),
                   ],
@@ -162,3 +189,4 @@ class _ArticleCard extends StatelessWidget {
     );
   }
 }
+
